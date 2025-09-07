826fedd32536673d0f6bc6da54df56e66be616e2
Alex <sasha_knyazev_2016@bk.ru>
2025-09-07 06:51:41 +0300

    create docker-compose.yml

--- src/main/java/com/example/auto/controller/AutoController.java
+++ src/main/java/com/example/auto/controller/AutoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 public class AutoController {
 
     private final AutoService autoService;
-роиорио
+
     @Autowired
     public AutoController(AutoService autoService) {
         this.autoService = autoService;
